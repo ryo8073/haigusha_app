6092e40018535d465ffbc8191c0a1baaab348a31
ryo8073 <136321452+ryo8073@users.noreply.github.com>
2025-04-18 11:10:49 +0900

    Add files via upload
    "Add layout.tsx to enable App Router"

---
+++ haigusha-app/app/layout.tsx
@@ -0,0 +1,9 @@
+// app/layout.tsx
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
